6e6eb7af97e11bdb2b436fbfe3aa353b29b4fc4b
Tanyaa-a <120506794+Tanyaa-a@users.noreply.github.com>
2024-08-03 22:29:28 -0700

    add week 6 assignment

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,23 +1,47 @@
 const Task = require('../models/Task')
-const getAllTasks = (req, res) => {
-  res.send("all tasks");
-};
+const asyncWrapper = require('../middleware/async')
+const { createCustomError } = require('../errors/custom-error')
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find({})
+  res.status(200).json({ tasks })
+})
 
-const createTask = async (req, res) => {
+const createTask = asyncWrapper(async (req, res) => {
   const task = await Task.create(req.body)
-  res.status(201).json({task});c
-};
-const getTask = (req, res) => {
-  res.json({id:req.params.id});
-};
+  res.status(201).json({ task })
+})
 
-const updateTask = (req, res) => {
-  res.send("update task");
-};
+const getTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskID } = req.params
+  const task = await Task.findOne({ _id: taskID })
+  if (!task) {
+    return next(createCustomError(`No task with id : ${taskID}`, 404))
+  }
 
-const deleteTask = (req, res) => {
-  res.send("delete task");
-};
+  res.status(200).json({ task })
+})
+const deleteTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskID } = req.params
+  const task = await Task.findOneAndDelete({ _id: taskID })
+  if (!task) {
+    return next(createCustomError(`No task with id : ${taskID}`, 404))
+  }
+  res.status(200).json({ task })
+})
+const updateTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskID } = req.params
+
+  const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+    new: true,
+    runValidators: true,
+  })
+
+  if (!task) {
+    return next(createCustomError(`No task with id : ${taskID}`, 404))
+  }
+
+  res.status(200).json({ task })
+})
 
 module.exports = {
   getAllTasks,
@@ -25,4 +49,4 @@ module.exports = {
   getTask,
   updateTask,
   deleteTask,
-};
+}
